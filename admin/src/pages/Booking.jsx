77864ed1b78d483e4c117d0f17a6aed7d30d58f2
Presha Karan <karanpresha@gmail.com>
2024-10-25 17:54:54 +0530

    added favicon

--- admin/src/pages/Booking.jsx
+++ admin/src/pages/Booking.jsx
@@ -4,13 +4,10 @@ import axios from 'axios';
 import { toast } from 'react-toastify'; 
 import Del from '../assets/delete.png';
 
-
-
 const Booking = ({ url }) => {
-
     const [reservations, setReservations] = useState([]);
-    const [cabinIds, setCabinIds] = useState([]);
     const [cabinList, setCabinList] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     const fetchCabinList = async () => {
         try {
@@ -24,13 +21,15 @@ const Booking = ({ url }) => {
     };
 
     const fetchReservations = async () => {
+        setLoading(true);
         try {
             const response = await axios.get(`http://localhost:5000/api/booking/list`);
             setReservations(response.data);
-            setCabinIds(response.data.map(reservation => reservation.cabinId));
         } catch (error) {
             console.error("Error fetching reservations:", error);
             toast.error("Error fetching reservations.");
+        } finally {
+            setLoading(false);
         }
     };
 
@@ -48,7 +47,7 @@ const Booking = ({ url }) => {
     useEffect(() => {
         fetchCabinList();
         fetchReservations();
-    }, []);
+    }, [url]);
 
     const formatDate = (dateString) => {
         const options = { day: 'numeric', month: 'long', year: 'numeric' };
@@ -65,42 +64,41 @@ const Booking = ({ url }) => {
                             <div className='container mx-auto p-4'>
                                 <h1 className="text-3xl font-bold mb-6">Bookings</h1>
                                 <div className='h-[60vh] overflow-y-auto scrollbar-hidden'>
-                                    {cabinIds.map((cabinId) => {
-                                        const currentReservation = reservations.find(reservation => reservation.cabinId === cabinId);
-                                        const cabin = cabinList.find(cabin => cabin.id === Number(cabinId));
- 
-
-                                        return (
-                                            cabin && currentReservation && (
-                                                <div key={currentReservation._id} className='flex border border-gray-500 w-full h-[18vh] mb-4 justify-between'>
-                                                    <div className='flex'>
-                                                        <img src={`${url}/images/${cabin.image}`} alt={cabin.name} className="w-32 h-[18vh] mr-4 object-cover" />
-                                                        <div className='flex flex-col justify-between'>
-                                                            <div className='flex justify-between'>
-                                                            <h1 className='text-2xl pl-2'>{currentReservation.numNights} nights in {cabin.name}</h1>
-                                                            <h1 className='text-2xl pl-2 text-gray-500 '>{currentReservation.userEmail}</h1>
-                                                            </div>
-                                                            <h1 className='text-xl p-2'>{formatDate(currentReservation.startDate)} - {formatDate(currentReservation.endDate)}</h1>
-                                                            <div className='flex justify-between w-full'>
-                                                                <div className='flex'>
-                                                                    <h1 className='text-xl p-2 ' style={{ color: '#aa8153' }}>Total: ${cabin.price * currentReservation.numNights + currentReservation.breakFastPrice}</h1>
-                                                                    <li className='list-none text-xl p-2 ml-10'>For {cabin.capacity} guests</li>
+                                    {loading ? (
+                                        <p>Loading...</p>
+                                    ) : (
+                                        reservations.map((currentReservation) => {
+                                            const cabin = cabinList.find(cabin => cabin.id === Number(currentReservation.cabinId));
+                                            
+                                            return (
+                                                cabin && (
+                                                    <div key={currentReservation._id} className='flex border border-gray-500 w-full h-[18vh] mb-4 justify-between'>
+                                                        <div className='flex'>
+                                                            <img src={`${url}/images/${cabin.image}`} alt={cabin.name} className="w-32 h-[18vh] mr-4 object-cover" />
+                                                            <div className='flex flex-col justify-between'>
+                                                                <div className='flex justify-between'>
+                                                                    <h1 className='text-2xl pl-2'>{currentReservation.numNights} nights in {cabin.name}</h1>
+                                                                    <h1 className='text-2xl pl-2 text-gray-500 '>{currentReservation.userEmail}</h1>
                                                                 </div>
-                                                                <h1 className='text-xl pt-2 pb-2 ml-[25vh] pl-10'>{formatDate(currentReservation.currentDate)}</h1>
+                                                                <h1 className='text-xl p-2'>{formatDate(currentReservation.startDate)} - {formatDate(currentReservation.endDate)}</h1>
+                                                                <div className='flex justify-between w-full'>
+                                                                    <div className='flex'>
+                                                                        <h1 className='text-xl p-2 ' style={{ color: '#aa8153' }}>Total: ${cabin.price * currentReservation.numNights + currentReservation.breakFastPrice}</h1>
+                                                                        <li className='list-none text-xl p-2 ml-10'>For {cabin.capacity} guests</li>
+                                                                    </div>
+                                                                    <h1 className='text-xl pt-2 pb-2 ml-[25vh] pl-10'>{formatDate(currentReservation.currentDate)}</h1>
+                                                                </div>
                                                             </div>
+                                                        </div>
+                                                        <div className='flex justify-between items-center mr-6 pl-6 border-l border-zinc-500 flex-col pt-6 pb-6'>
+                                                            <img src={Del} alt='delete' className='w-10 h-10 cursor-pointer' onClick={() => handleDelete(currentReservation._id)} />
+                                                            <div className='text-gray-500 cursor-pointer' onClick={() => handleDelete(currentReservation._id)}>Delete</div>
                                                         </div>
                                                     </div>
-                                                    <div className='flex justify-between items-center mr-6 pl-6 border-l border-zinc-500 flex-col pt-6 pb-6'>
-                                                            <img src={Del} alt='delete' className='w-10 h-10' />
-                                                        <div  onClick={() => handleDelete(currentReservation._id)}>
-                                                            Delete
-                                                        </div>
-                                                        </div>
-                                                    
-                                                </div>
-                                            )
-                                        );
-                                    })}
+                                                )
+                                            );
+                                        })
+                                    )}
                                 </div>
                             </div>
                         </main>
