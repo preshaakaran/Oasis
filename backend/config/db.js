02f664bf9e0c8753b8eef3864bc1c0c3a56be502
Presha Karan <159997342+preshaakaran@users.noreply.github.com>
2025-01-24 01:31:00 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     await mongoose.connect(process.env.MONGODB_URI).then(() => console.log("connected to database")).catch((err) => console.log(err));
-<<<<<<< HEAD
+
    
 }
-=======
-        
-}
->>>>>>> 5825f4f383cf969de1ffa36a93cd78bbc6452e72
+
